aa3f2e83967f4d07ed1eec1e371063d3ff0bee6f
pavelgor84 <pavelgor@gmail.com>
2022-04-23 20:16:48 +0400

    first commit

---
+++ contracts/DappToken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract DappToken is ERC20 {
+    constructor() ERC20("Dapp Token", "DAPP") {
+        _mint(msg.sender, 100000000000000000000000);
+    }
+}
