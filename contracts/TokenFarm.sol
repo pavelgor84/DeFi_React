c6346bfe7236c8443cb35a91a0e06f24466c0ab1
pavelgor84 <pavelgor@gmail.com>
2022-04-26 20:50:03 +0400

    unique stakers

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 contract TokenFarm is Ownable {
     //stake tokens
@@ -12,10 +13,26 @@ contract TokenFarm is Ownable {
     //getEthValue
 
     address[] public allowedTokens;
+    mapping(address => mapping(address => uint256)) public stakingBalance; //token_address => user_address(wallet) => ammount
+    mapping(address => uint256) public uniqueTokenStaked;
+    address[] stakers;
 
     function stakeTokens(uint256 _amount, address _token) public {
         require(_amount > 0, "The amount must be greather than 0");
-        require(isTokenAllowed(_token) == true);
+        require(isTokenAllowed(_token), "This token is currently not allowed");
+        //transferFrom
+        IERC20(_token).transferFrom(msg.sender, address(this), _amount);
+        stakingBalance[_token][msg.sender] =
+            stakingBalance[_token][msg.sender] +
+            _amount;
+        checkUniqueStaker(msg.sender, _token);
+    }
+
+    function checkUniqueStaker(address user, address token) {
+        if (stakingBalance[token][user] <= 0) {
+            uniqueTokenStaked[user] = uniqueTokenStaked[user] + 1;
+            stakers.push(msg.sender);
+        }
     }
 
     function addAllowedTokens(address _token) public onlyOwner {
