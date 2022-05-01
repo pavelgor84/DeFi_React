9d1313ef2880f316c7d377567728935560167ddd
pavelgor84 <pavelgor@gmail.com>
2022-05-01 14:16:03 +0400

    add allowed tokens

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,3 +1,4 @@
+from multiprocessing import allow_connection_pickling
 from scripts.helpful_scripts import get_account, get_contract
 from brownie import DappToken, TokenFarm, network, config
 from web3 import Web3
@@ -7,7 +8,7 @@ KEPT_BALANCE = Web3.toWhei(100, "ether")
 
 def deploy_token_farm_and_dapp_token():
     account = get_account()
-    dappToken = DappToken.deploy({"from": account})
+    dapp_token = DappToken.deploy({"from": account})
     tokenFarm = TokenFarm.deploy(
         dappToken.address,
         {"from": account},
@@ -18,9 +19,25 @@ def deploy_token_farm_and_dapp_token():
     )  # Add own DappTokens to this contract
     tx.wait(1)
 
+    # dapp token, weth token, fau token or DAI
+    weth_token = get_contract("weth_token")
+    fau_token = get_contract("fau_token")
+    allowed_token_dictionary = {
+        weth_token: get_contract("eth_usd_price_feed"),
+        fau_token: get_contract("dai_usd_price_feed"),
+        dapp_token: get_contract("dai_usd_price_feed"),
+    }
 
-def add_allowed_tokens():
-    pass
+    add_allowed_tokens(tokenFarm, allowed_token_dictionary, account)
+
+
+def add_allowed_tokens(token_farm, token_dictionary, account):
+    for token in token_dictionary:
+        tx_allow = token_farm.addAllowedTokens(token.address, {"from": account})
+        tx_allow.wait(1)
+        tx_feed = token_farm.addPriceFeed(
+            token.address, token_dictionary[token], {"from": account}
+        )
 
 
 def main():
