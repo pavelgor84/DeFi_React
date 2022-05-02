ab23d1c42ed4de5dbf8fc2bdd330425c2cb2e673
pavelgor84 <pavelgor@gmail.com>
2022-05-02 20:51:53 +0400

    test stake tokens

---
+++ tests/unit/test_token_farm.py
@@ -0,0 +1,47 @@
+from email.headerregistry import Address
+import pytest
+from brownie import network, exceptions
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    get_account,
+    get_contract,
+)
+from scripts.deploy import deploy_token_farm_and_dapp_token
+
+
+def test_add_price_feed():
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    fake_account = get_account(index=1)
+    account = get_account()
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+    # Act
+    price_feed_address = get_contract("eth_usd_price_feed")
+    tx = token_farm.addPriceFeed(
+        dapp_token.address, price_feed_address, {"from": account}
+    )
+    tx.wait(1)
+    # Assert
+    assert token_farm.tokenPriceFeed(dapp_token.address) == get_contract(
+        "eth_usd_price_feed"
+    )
+    with pytest.raises(exceptions.VirtualMachineError):
+        tx = token_farm.addPriceFeed(
+            dapp_token.address, price_feed_address, {"from": fake_account}
+        )
+
+
+def test_stake_tokens(amount_staked):
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    account = get_account()
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+    # Act
+    dapp_token.approve(token_farm.address, amount_staked, {"from": account})
+    token_farm.stakeTokens(amount_staked, dapp_token.address, {"from": account})
+    # Assert
+    assert (
+        token_farm.stakingBalance(dapp_token.address, account.address) == amount_staked
+    )
