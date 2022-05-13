d835804ce8136f494adc7922ce4fb3dc1c4d7380
pavelgor84 <pavelgor@gmail.com>
2022-05-13 14:09:36 +0400

    deploying on Kovan

--- front_end/src/components/Header.tsx
+++ front_end/src/components/Header.tsx
@@ -1,14 +1,24 @@
 import { useEthers } from "@usedapp/core";
 import React from "react";
+import { Button, makeStyles } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+    container: {
+        padding: theme.spacing(1),
+        display: "flex",
+        justifyContent: "flex-end",
+        gap: theme.spacing(1)
+    }
+}))
 
 export default function Header() {
     const { account, activateBrowserWallet, deactivate } = useEthers()
+    const classes = useStyles();
 
     return (
-        <div>
+        <div className={classes.container}>
             <div>
-                <button onClick={() => activateBrowserWallet()}>Connect</button>
+                <Button variant="text" onClick={() => activateBrowserWallet()}>Connect</Button>
             </div>
             {account && <p>Account: {account}</p>}
         </div>
--- front_end/src/index.tsx
+++ front_end/src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Container } from '@material-ui/core';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <App />
+        <Container maxWidth="md">
+            <App />
+        </Container>
     </React.StrictMode>
 );
 
