75b719133600237ac4574670902ab12ecace9017
pavelgor84 <pavelgor@gmail.com>
2022-05-24 16:47:17 +0400

    tetmporary getTokenBalance solution

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { DAppProvider, ChainId } from "@usedapp/core"
+import { DAppProvider, Config, Kovan } from "@usedapp/core"
 import Header from './components/Header';
 import Main from './components/Main';
+import { getDefaultProvider } from 'ethers';
+
+const config: Config = {
+  readOnlyChainId: Kovan.chainId,
+  readOnlyUrls: {
+    [Kovan.chainId]: getDefaultProvider('kovan'),
+  },
+}
 
 export default function App() {
 
-
   return (
-    <DAppProvider config={{}}>
+    <DAppProvider config={config}>
       <Header />
       <Main />
     </DAppProvider>
